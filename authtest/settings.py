f680837acfc1b38474f365d3bf51bf7dfecc0005
Hoshimon <etsukohoshi1997@gmail.com>
2017-04-04 12:54:13 -0300

    Auth done

--- authtest/settings.py
+++ authtest/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'authtest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/' #This brings to the home view
